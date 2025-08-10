440cee30ffbf84dfd4fe5ba774ac1dec41fb242a
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-08-10 14:43:20 -0600

    Update Ejercicio_Ago8.c

--- Semana1/Ejercicio_Ago8.c
+++ Semana1/Ejercicio_Ago8.c
@@ -3,16 +3,17 @@
 #include <math.h>
 
 int main(){
-  float x,a,b,c;
+  float x;
   float X[3];
-  printf("Introducir un numero decimal");
+  printf("Introducir un numero decimal: ");
   scanf("%f",&x);
-  a = x*x;
-  b = log10f(x);
-  c = expf(x) + cosf(x);
-  a = X[0];
-  b = X[1];
-  c = X[2];
-  printf("%f %f %f \n",a,b,c);
+  
+  X[0] = x*x;
+  X[1] = log10f(x);
+  X[2] = expf(x) + cosf(x); 
+  printf("%f %f %f \n", X[0], X[1], X[2]);
+
+  return 0;
 
 }
+
