35419fc9f732f438b83574dd944fc65633ecd74e
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-08-15 11:50:17 -0600

    Add files via upload

---
+++ Sema__2/Raices.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h> 
+int main(){
+  float a,b,c,x1,x2;
+  printf("ingresar los coeficientes de la ecuación cuadrática en órden \n");
+  scanf("%f", &a);
+  scanf("%f", &b);
+  scanf("%f", &c);
+  if(a==0){
+    if(b!=0){
+      x1=-c/b;
+      printf("x1 = \n",x1);
+    }
+    else printf("la solución de la ecuación es trivial");
+  }
+  else{
+    D = 
+  }
+
+  return 0;
+}
