65e5e5b9acbfb3cd3fba6870e023c09378f1afb7
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-08-16 18:32:15 -0600

    Update Raices.c

--- Sema__2/Raices.c
+++ Sema__2/Raices.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h> 
 int main(){
-  float a,b,c,x1,x2;
+  float a,b,c,x1,x2,D,R,i;
   printf("ingresar los coeficientes de la ecuación cuadrática en órden \n");
   scanf("%f", &a);
   scanf("%f", &b);
@@ -10,11 +10,31 @@ int main(){
     if(b!=0){
       x1=-c/b;
       printf("x1 = \n",x1);
-    }
-    else printf("la solución de la ecuación es trivial");
-  }
-  else{
-    D = 
+    } else{
+      printf("x1 = 0 \n", c);
+    } else{
+      if(c==0){
+        printf("Infinitas raíces reales\n");
+      } else{
+        printf("Las raíces son complejas");
+        }
+      }
+  } else{
+    D = b*b - 4*a*c; 
+    if(D>0){
+      x1 = (-b + sqrt(D)) / (2*a);
+      x2 = (-b - sqrt(D)) / (2*a);
+      printf("x1 = %f \n x2 = %f\n", x1, x2);
+      } else{
+        if(D == 0){
+          x1 = -b / (2*a);
+          printf("x1 = %f\n", x1);
+        } else{
+          R = -b / (2*a);
+          i = sqrt(-D) / (2*a);
+          printf("x1 = %f + %fi \n x2 = %f - %fi \n", R, i, R, i);
+          }
+        }
   }
 
   return 0;
