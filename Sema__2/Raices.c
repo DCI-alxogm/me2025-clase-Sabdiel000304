7175f3f80f78b43fefa05cbb63a1a2a966f30764
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-08-16 18:36:01 -0600

    Update Raices.c

--- Sema__2/Raices.c
+++ Sema__2/Raices.c
@@ -9,33 +9,30 @@ int main(){
   if(a==0){
     if(b!=0){
       x1=-c/b;
-      printf("x1 = \n",x1);
-    } else{
-      printf("x1 = 0 \n", c);
+      printf("x1 = %f\n",x1);
     } else{
       if(c==0){
-        printf("Infinitas raíces reales\n");
+      printf("Infinitas raíces reales\n");
       } else{
-        printf("Las raíces son complejas");
+        printf("Las raíces son complejas\n");
         }
       }
-  } else{
-    D = b*b - 4*a*c; 
+  }else{
+    D = b*b - 4*a*c;
     if(D>0){
-      x1 = (-b + sqrt(D)) / (2*a);
-      x2 = (-b - sqrt(D)) / (2*a);
-      printf("x1 = %f \n x2 = %f\n", x1, x2);
-      } else{
-        if(D == 0){
-          x1 = -b / (2*a);
-          printf("x1 = %f\n", x1);
+    x1 = (-b + sqrt(D)) / (2*a);
+    x2 = (-b - sqrt(D)) / (2*a);
+    printf("x1 = %f \n x2 = %f\n", x1, x2);
+    } else{
+      if(D == 0){
+        x1 = -b / (2*a);
+        printf("x1 = %f\n", x1);
         } else{
           R = -b / (2*a);
           i = sqrt(-D) / (2*a);
           printf("x1 = %f + %fi \n x2 = %f - %fi \n", R, i, R, i);
           }
-        }
-  }
-
-  return 0;
+      } 
+    }
+return 0;
 }
