cb9fc11199a4e6399801b6c2398664be9cc5e44e
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-08-19 11:55:19 -0600

    Add files via upload

---
+++ Semana_3/presicion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+  int n;
+  float Ea,x,a;
+  for(i=0; i++; i=n){
+    printf("Programa que calcula la serie de Mc Laurin para e^x\n");
+    printf("tomando en cuenta el error aproximado")
+    printf("ingresa el número de iteraciones: \n");
+    scanf(&n);
+    printf("ingresa la variable x\n")
+    scanf(&x);
+    pow(e,x)=A;
+    (A-a)/A = Ea;
+    printf("n= %d , e^x= , Ea=",&n ,&a, &Ea);
+
+
+  }
+
+  return 0;
+}
