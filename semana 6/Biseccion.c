7c7903fc304d467938ae94dde3647d66145a4030
Sabdiel000304 <78510061+Sabdiel000304@users.noreply.github.com>
2025-09-09 11:48:32 -0600

    Add files via upload

---
+++ semana 6/Biseccion.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <math.h>
+float f(float c);
+int main (){
+  float a,b;
+int k
+k=(a+b)/2;
+if (f(k)*f(a)){
+  b=k;
+}
+else a=k;
+}
+float f(float c){
+  return (9.81*68.1)/c*(1-exp(-(c/68.1)*10))-40;
+}
+
+return 0;
